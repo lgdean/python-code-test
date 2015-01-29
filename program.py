7fdc4077dc6e11968b4d44e3bd830c26ffb6af34
Laura Dean <lgdean@users.noreply.github.com>
2015-01-29 01:13:34 -0500

    removed unused imports and camelCase

--- program.py
+++ program.py
@@ -1,14 +1,12 @@
-import sys
 import datetime
-import operator
 
-def stringifyPerson(pTuple):
-    hackTuple = (pTuple[0], pTuple[1], pTuple[2],
-                 "{dt.month}/{dt.day}/{dt.year}".format(dt=pTuple[3]),
-                 pTuple[4])
-    return " ".join(map(str, hackTuple)) # last first gender dob color
+def stringify_person(p_tuple):
+    hack_tuple = (p_tuple[0], p_tuple[1], p_tuple[2],
+                 "{dt.month}/{dt.day}/{dt.year}".format(dt=p_tuple[3]),
+                 p_tuple[4])
+    return " ".join(map(str, hack_tuple)) # last first gender dob color
 
-def dateFromMDY(strs):
+def date_from_mdy(strs):
     ints = map(int, strs)
     return datetime.date(ints[2], ints[0], ints[1])
 
@@ -18,19 +16,19 @@ def do_it():
         for line in f:
             parts = [l.strip() for l in line.split(',')] # hm, prefer map?
             people.append((parts[0], parts[1], parts[2],
-                           dateFromMDY(parts[4].split('/')), parts[3]))
+                           date_from_mdy(parts[4].split('/')), parts[3]))
     with open("input_files/pipe.txt","r") as f:
         for line in f:
             parts = [l.strip() for l in line.split('|')] # hm, prefer map?
             people.append((parts[0], parts[1],
                            "Female" if parts[3][0] == "F" else "Male",
-                           dateFromMDY(parts[5].split('-')), parts[4]))
+                           date_from_mdy(parts[5].split('-')), parts[4]))
     with open("input_files/space.txt","r") as f:
         for line in f:
             parts = [l.strip() for l in line.split(' ')] # hm, prefer map?
             people.append((parts[0], parts[1],
                            "Female" if parts[3][0] == "F" else "Male",
-                           dateFromMDY(parts[4].split('-')), parts[5]))
+                           date_from_mdy(parts[4].split('-')), parts[5]))
     sorts = [
         {'key': lambda p: (p[2],p[0])},
         {'key': lambda p: p[3]},
@@ -40,7 +38,7 @@ def do_it():
     for idx,sorter in enumerate(sorts):
         output.append("Output %d:"%(idx+1))
         sorted_people = sorted(people, **sorter)
-        output.extend(map(stringifyPerson, sorted_people))
+        output.extend(map(stringify_person, sorted_people))
         output.append("")
     return output
 
