e138b6701f4d0a3609519322a6d711f75ff6d802
Laura Dean <lgdean@users.noreply.github.com>
2015-01-29 21:45:47 -0500

    now have Person. thanks to rchasin for suggesting playing with namedtuple.

--- program.py
+++ program.py
@@ -1,9 +1,14 @@
 import datetime
+from collections import namedtuple
 
-def stringify_person(p_tuple):
-    hack_tuple = (p_tuple[0], p_tuple[1], p_tuple[2],
-                 "{dt.month}/{dt.day}/{dt.year}".format(dt=p_tuple[3]),
-                 p_tuple[4])
+# Cool feature, but I might not want to expose the tuple-ness (and the
+# order of fields) in the real world. But I do like the immutability!
+Person = namedtuple('Person', ['last', 'first', 'gender', 'dob', 'fav_color'])
+
+def stringify_person(p):
+    hack_tuple = (p.last, p.first, p.gender,
+                  "{dt.month}/{dt.day}/{dt.year}".format(dt=p.dob),
+                  p.fav_color)
     return " ".join(map(str, hack_tuple)) # last first gender dob color
 
 def date_from_mdy(strs):
@@ -15,24 +20,24 @@ def do_it():
     with open("input_files/comma.txt","r") as f:
         for line in f:
             parts = [l.strip() for l in line.split(',')] # hm, prefer map?
-            people.append((parts[0], parts[1], parts[2],
-                           date_from_mdy(parts[4].split('/')), parts[3]))
+            people.append(Person(parts[0], parts[1], parts[2],
+                                 date_from_mdy(parts[4].split('/')), parts[3]))
     with open("input_files/pipe.txt","r") as f:
         for line in f:
             parts = [l.strip() for l in line.split('|')] # hm, prefer map?
-            people.append((parts[0], parts[1],
-                           "Female" if parts[3][0] == "F" else "Male",
-                           date_from_mdy(parts[5].split('-')), parts[4]))
+            people.append(Person(parts[0], parts[1],
+                                 "Female" if parts[3][0] == "F" else "Male",
+                                 date_from_mdy(parts[5].split('-')), parts[4]))
     with open("input_files/space.txt","r") as f:
         for line in f:
             parts = [l.strip() for l in line.split(' ')] # hm, prefer map?
-            people.append((parts[0], parts[1],
-                           "Female" if parts[3][0] == "F" else "Male",
-                           date_from_mdy(parts[4].split('-')), parts[5]))
+            people.append(Person(parts[0], parts[1],
+                                 "Female" if parts[3][0] == "F" else "Male",
+                                 date_from_mdy(parts[4].split('-')), parts[5]))
     sorts = [
-        {'key': lambda p: (p[2],p[0])},
-        {'key': lambda p: p[3]},
-        {'key': lambda p: p[0], 'reverse': True}
+        {'key': lambda p: (p.gender,p.last)},
+        {'key': lambda p: p.dob},
+        {'key': lambda p: p.last, 'reverse': True}
     ]
     output = []
     for idx,sorter in enumerate(sorts):
